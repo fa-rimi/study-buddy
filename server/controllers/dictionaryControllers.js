da9c9e72bbf8d2656a368d0ed6c5f00c68f7e417
rimi <a.fatimaarimi@gmail.com>
2023-09-26 16:38:30 -0400

    completed update word and delete word

---
+++ server/controllers/dictionaryControllers.js
@@ -0,0 +1,87 @@
+const Dictionary = require("../models/dictionary"); // Make sure to import your Dictionary model
+
+const newEntry = async (req, res) => {
+  try {
+    // Extract the word data from the request body
+    const { word, definition, example } = req.body;
+
+    // Create a new word entry
+    const newWord = new Dictionary({
+      word,
+      definition,
+      example,
+    });
+
+    // Save the new word entry to the database
+    await newWord.save();
+
+    // Send a success response
+    res
+      .status(201)
+      .json({ message: "Word created successfully", word: newWord });
+  } catch (error) {
+    console.error("Error creating word:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const displayWords = async (req, res) => {
+  try {
+    // Fetch all words from the database
+    const words = await Dictionary.find();
+
+    // Send the words as a JSON response
+    res.json(words);
+  } catch (error) {
+    console.error("Error fetching words:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const updateWord = async (req, res) => {
+  try {
+    const { _id } = req.params; // Assuming you pass the word ID as a route parameter
+    const { word, definition, example } = req.body; // Assuming you send the updated word data in the request body
+
+    // Find the word by ID and update it
+    const updatedWord = await Dictionary.findByIdAndUpdate(
+      _id,
+      { word, definition, example },
+      { new: true }
+    );
+
+    if (!updatedWord) {
+      return res.status(404).json({ error: "Word not found" });
+    }
+
+    res.status(200).json(updatedWord);
+  } catch (error) {
+    console.error("Error updating word:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const deleteWord = async (req, res) => {
+  try {
+    const { _id } = req.params; // Assuming you pass the word ID as a route parameter
+
+    // Find the word by ID and remove it
+    const deletedWord = await Dictionary.findByIdAndRemove(_id);
+
+    if (!deletedWord) {
+      return res.status(404).json({ error: "Word not found" });
+    }
+
+    res.status(200).json({ message: "Word deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting word:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = {
+  newEntry,
+  displayWords,
+  updateWord,
+  deleteWord,
+};
